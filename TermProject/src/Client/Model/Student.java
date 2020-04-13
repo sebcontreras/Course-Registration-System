9de5fdc5b7056f86dc4e8ea4b654aac2a3aaba9c
sebcontreras <seb.contreras24@gmail.com>
2020-04-12 21:01:12 -0600

    added's seb's branch attempt 2

--- TermProject/src/Client/Model/Student.java
+++ TermProject/src/Client/Model/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	
 	public static void main(String[] args) {
 		System.out.println("This is a test on a new branch");
-		System.out.println("Seb's branch added");
+		System.out.println("Seb's branch added.");
 
 	}
 }
