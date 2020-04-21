349a07d9b6030d4e4fe1893888871e16ba40511b
Michael Vassilev <Mikey.Vassilev@gmail.com>
2020-04-20 20:42:07 -0600

    javadocs

--- TermProject/src/Server/Controller/Server.java
+++ TermProject/src/Server/Controller/Server.java
@@ -6,13 +6,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-
+/**
+ * The purpose of this class is to represent the server side of this server/client application and run the server
+ * @author Michael Vassilev 30068475, Vic Phan 30061885, Sebastian Contreras 30062418
+ *
+ */
 public class Server {
-	
+	/*
+	 * the connection to the client side
+	 */
 	private ServerSocket serverSocket;
-	
+	/*
+	 * the pool of the threds that allows the application handle multiple clients at the same time
+	 */
 	private ExecutorService pool;
-	
+	/**
+	 * constructs an instance of Server that initiates the ServerSocket and thread pool
+	 * @param portNumber
+	 */
 	public Server (int portNumber) {
 		try {
 			serverSocket = new ServerSocket(portNumber);
@@ -22,7 +33,10 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * this method starts the application on the server side by loading the information from the database to the program
+	 * as well as waiting for the connection from the client side. Lastly, it continuously waits for input from the client side
+	 */
 	public void start() {
 		try {
 			DBController database = new DBController();
--- TermProject/src/Server/Model/CourseOffering.java
+++ TermProject/src/Server/Model/CourseOffering.java
@@ -1,21 +1,47 @@
 package Server.Model;
 
 import java.util.ArrayList;
-//
+/**
+ * The purpose of this class is to represent course offerings of the courses in the database
+ * @author Michael Vassilev 30068475, Vic Phan 30061885, Sebastian Contreras 30062418
+ *
+ */
 public class CourseOffering {
-
+	/*
+	 * the section number
+	 */
 	private int secNum;
+	/*
+	 * the section capacity
+	 */
 	private int secCap;
+	/*
+	 * the course of this course offering
+	 */
 	private Course theCourse;
+	/*
+	 * the list of registrations in this course offering
+	 */
 	private ArrayList <Registration> offeringRegList;
+	/*
+	 * the checker for a full course offering
+	 */
 	private boolean full;
-	
+	/**
+	 * constructs an instance if CourseOffering and sets the section and capacity,
+	 * also initiates the registration list
+	 * @param secNum
+	 * @param secCap
+	 */
 	public CourseOffering (int secNum, int secCap) {
 		this.setSecNum(secNum);
 		this.setSecCap(secCap);
 		offeringRegList = new ArrayList <Registration>();
 	}
-	
+	/**
+	 * this method checks id the course offering is full
+	 * @return true if full, false otherwise
+	 */
 	public boolean isFull() {
 		if (offeringRegList.size()<secCap) {
 			full=false;
@@ -26,31 +52,61 @@ public class CourseOffering {
 		return full;
 	}
 	
-	
+	/**
+	 * this method is the getter for the section number
+	 * @return the section number
+	 */
 	public int getSecNum() {
 		return secNum;
 	}
+	/**
+	 * this method is the setter for the section number
+	 * @param secNum, the section number
+	 */
 	public void setSecNum(int secNum) {
 		this.secNum = secNum;
 	}
+	/**
+	 * this method is the getter for the capacity
+	 * @return the capacity
+	 */
 	public int getSecCap() {
 		return secCap;
 	}
+	/**
+	 * this method is the setter for the capacity
+	 * @param secCap, the capacity
+	 */
 	public void setSecCap(int secCap) {
 		this.secCap = secCap;
 	}
+	/**
+	 * this method is the getter for the course
+	 * @return the course
+	 */
 	public Course getTheCourse() {
 		return theCourse;
 	}
+	/**
+	 * this method is the setter for the course
+	 * @param theCourse, the course
+	 */
 	public void setTheCourse(Course theCourse) {
 		this.theCourse = theCourse;
 	}
+	/**
+	 * this method converts the course offering into a String and returns it
+	 */
 	@Override
 	public String toString () {
 		String st = getTheCourse().getCourseName() + " " + getTheCourse().getCourseNum() + "\n";
 		st += "Section Num: " + getSecNum() + ", section cap: "+ getSecCap() +"\n";
 		return st;
 	}
+	/**
+	 * this method adds the given registration to the registration list
+	 * @param registration, the registration to be added
+	 */
 	public void addRegistration(Registration registration) {
 		offeringRegList.add(registration);
 	}
